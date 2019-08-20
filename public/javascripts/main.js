dc78c97bc407d79c82bf0394aecf028c69903d97
jurgen <jurgenporubenger@gmail.com>
2019-08-20 14:33:42 +0300

    20.08-2

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,14 +3,10 @@ console.log("Alive!");
 
 document.querySelector(".btn").addEventListener("click", function (e) {
     e.preventDefault();
-    //получаем данные формы
-    // let registerForm = document.forms["registerForm"];
-    // let car = registerForm.elements["car"].value;
-    // let model = registerForm.elements["model"].value;
-    // let img = registerForm.elements["img"].value;
-    let car = document.querySelector(".car").value;
-    let model = document.querySelector(".model").value;
-    let img = document.querySelector(".img").value;
+    //получаем данные полей ввода
+    let car = document.querySelector("#car").value;
+    let model = document.querySelector("#model").value;
+    let img = document.querySelector("#img").value;
     // сериализуем данные в json
     let user = JSON.stringify({car: car, model: model, img: img});
     console.log(user);
@@ -21,11 +17,10 @@ document.querySelector(".btn").addEventListener("click", function (e) {
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.addEventListener("load", function () {
         let receivedUser = JSON.parse(xhr.response);
-
         //получаем и парсим ответ сервера
-        // console.log("receivedUser.userName, "-", receivedUser.userAge");   // смотрим ответ сервера
         console.log(receivedUser);
         // console.log(xhr.response);
     });
+    //отправляем на сервер данные из инпутов
     xhr.send(user);
 });
